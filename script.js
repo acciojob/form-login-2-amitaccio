950c23e229ad406dc313a4cac4598f567d09a3b4
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-29 18:21:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let formElm = document.getElementById("form");
+formElm.addEventListener("submit" , (event)=>{
+	event.preventDefault();
+	alert(  "First Name:" + formElm.firstName.value);
+	 alert(  "Last Name:" + formElm.lastname.value);
+	 alert(  "Phone Number:" + formElm.phno.value);
+	alert(  "Email ID:" + formElm.email.value);
+})
